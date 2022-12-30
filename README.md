3bb8b45f0731049a4629dff8a1a45c6942dd8936
nont <nontkrub@gmail.com>
2022-12-29 20:24:34 -0800

    Add lesson 2

---
+++ README.md
@@ -0,0 +1,4 @@
+# AZ-700 Designing and Implementing Microsoft Azure Networking Solutions
+
+Instead of doing the labs on the portal, I'm using Terraform to accomplish the
+same thing (why not).
--- main.tf
+++ main.tf
@@ -16,14 +16,62 @@ resource "azurerm_resource_group" "nontnw" {
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "rg-eus-m01" {
-  name                = "rg-eus-m01"
-  resource_group_name = azurerm_resource_group.nontnw
-  location            = azurerm_resource_group.nontnw.location
-  address_space       = ["172.20.0.0/16"]
+resource "azurerm_virtual_network" "core_services_vnet" {
+  name                = "CoreServicesVnet"
+  resource_group_name = azurerm_resource_group.nontnw.name
+  location            = "eastus"
+  address_space       = ["10.30.0.0/16"]
 
   subnet {
-    name = "web"
-    address_prefix = "172.20.1.0/24"
+    name           = "WebSubnet"
+    address_prefix = "10.30.30.0/24"
+  }
+  subnet {
+    name           = "SharedSubnet"
+    address_prefix = "10.30.10.0/24"
+  }
+  subnet {
+    name           = "DatabaseSubnet"
+    address_prefix = "10.30.20.0/24"
+  }
+  subnet {
+    name           = "GatewaySubnet"
+    address_prefix = "10.30.0.0/27"
   }
 }
+
+resource "azurerm_virtual_network" "manufacturing_vnet" {
+  name                = "ManufacturingVnet"
+  resource_group_name = azurerm_resource_group.nontnw.name
+  location            = "westeurope"
+  address_space       = ["10.40.0.0/16"]
+
+  subnet {
+    name           = "ManufactSubnet"
+    address_prefix = "10.40.10.0/24"
+  }
+  subnet {
+    name           = "SensorSubnet1"
+    address_prefix = "10.40.20.0/24"
+  }
+  subnet {
+    name           = "SensorSubnet2"
+    address_prefix = "10.40.21.0/24"
+  }
+  subnet {
+    name           = "SensorSubnet3"
+    address_prefix = "10.40.22.0/24"
+  }
+}
+
+resource "azurerm_virtual_network" "research_vnet" {
+  name = "ResearchVnet"
+  resource_group_name = azurerm_resource_group.nontnw.name
+  location            = "southeastasia"
+  address_space       = ["10.20.0.0/16"]
+  subnet {
+    name           = "ResearchSubnet"
+    address_prefix = "10.20.0.0/24"
+  }
+}
+
