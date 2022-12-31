30f88f225babe5c752ab258c2c6eab6a453bf92d
nont <nontkrub@gmail.com>
2022-12-31 00:16:31 -0800

    Add lesson 4 - network peering

--- vnets.tf
+++ vnets.tf
@@ -47,7 +47,7 @@ resource "azurerm_virtual_network" "manufacturing_vnet" {
 }
 
 resource "azurerm_virtual_network" "research_vnet" {
-  name = "ResearchVnet"
+  name                = "ResearchVnet"
   resource_group_name = azurerm_resource_group.nontnw.name
   location            = "southeastasia"
   address_space       = ["10.20.0.0/16"]
@@ -57,3 +57,20 @@ resource "azurerm_virtual_network" "research_vnet" {
   }
 }
 
+resource "azurerm_virtual_network_peering" "core_to_manufacturing" {
+  name                         = "CoreServicesVnet-to-ManufacturingVnet"
+  resource_group_name          = azurerm_resource_group.nontnw.name
+  virtual_network_name         = azurerm_virtual_network.core_services_vnet.name
+  remote_virtual_network_id    = azurerm_virtual_network.manufacturing_vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+}
+
+resource "azurerm_virtual_network_peering" "manufacturing_to_core" {
+  name                         = "ManufacturingVnet-to-CoreServicesVnet"
+  resource_group_name          = azurerm_resource_group.nontnw.name
+  virtual_network_name         = azurerm_virtual_network.manufacturing_vnet.name
+  remote_virtual_network_id    = azurerm_virtual_network.core_services_vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+}
