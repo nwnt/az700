d1dcd8d14d44ae3b6c71dc4b05886cddce006083
nont <nontkrub@gmail.com>
2023-01-03 09:13:08 -0800

    Add the missing options
    The names are the not the same as the current documentation

--- vnets.tf
+++ vnets.tf
@@ -10,24 +10,32 @@ resource "azurerm_subnet" "core_gateway" {
   resource_group_name                           = azurerm_resource_group.nontnw.name
   virtual_network_name                          = azurerm_virtual_network.core_services.name
   address_prefixes                              = ["10.20.0.0/27"]
+  enforce_private_link_service_network_policies = true
+  enforce_private_link_endpoint_network_policies = true
 }
 resource "azurerm_subnet" "core_shared_services" {
   name                                          = "SharedServicesSubnet"
   resource_group_name                           = azurerm_resource_group.nontnw.name
   virtual_network_name                          = azurerm_virtual_network.core_services.name
   address_prefixes                              = ["10.20.10.0/24"]
+  enforce_private_link_service_network_policies = true
+  enforce_private_link_endpoint_network_policies = true
 }
 resource "azurerm_subnet" "core_database" {
   name                                          = "DatabaseSubnet"
   resource_group_name                           = azurerm_resource_group.nontnw.name
   virtual_network_name                          = azurerm_virtual_network.core_services.name
   address_prefixes                              = ["10.20.20.0/24"]
+  enforce_private_link_service_network_policies = true
+  enforce_private_link_endpoint_network_policies = true
 }
 resource "azurerm_subnet" "core_webservices" {
   name                                          = "PublicWebServiceSubnet"
   resource_group_name                           = azurerm_resource_group.nontnw.name
   virtual_network_name                          = azurerm_virtual_network.core_services.name
   address_prefixes                              = ["10.20.30.0/24"]
+  enforce_private_link_service_network_policies = true
+  enforce_private_link_endpoint_network_policies = true
 }
 
 resource "azurerm_virtual_network" "manufacturing_vnet" {
@@ -41,24 +49,32 @@ resource "azurerm_subnet" "manufacturing_system" {
   resource_group_name                           = azurerm_resource_group.nontnw.name
   virtual_network_name                          = azurerm_virtual_network.manufacturing_vnet.name
   address_prefixes                              = ["10.30.10.0/24"]
+  enforce_private_link_service_network_policies = true
+  enforce_private_link_endpoint_network_policies = true
 }
 resource "azurerm_subnet" "manufacturing_sensor_1" {
   name                                          = "SensorSubnet1"
   resource_group_name                           = azurerm_resource_group.nontnw.name
   virtual_network_name                          = azurerm_virtual_network.manufacturing_vnet.name
   address_prefixes                              = ["10.30.20.0/24"]
+  enforce_private_link_service_network_policies = true
+  enforce_private_link_endpoint_network_policies = true
 }
 resource "azurerm_subnet" "manufacturing_sensor_2" {
   name                                          = "SensorSubnet2"
   resource_group_name                           = azurerm_resource_group.nontnw.name
   virtual_network_name                          = azurerm_virtual_network.manufacturing_vnet.name
   address_prefixes                              = ["10.30.21.0/24"]
+  enforce_private_link_service_network_policies = true
+  enforce_private_link_endpoint_network_policies = true
 }
 resource "azurerm_subnet" "manufacturing_sensor_3" {
   name                                          = "SensorSubnet3"
   resource_group_name                           = azurerm_resource_group.nontnw.name
   virtual_network_name                          = azurerm_virtual_network.manufacturing_vnet.name
   address_prefixes                              = ["10.30.22.0/24"]
+  enforce_private_link_service_network_policies = true
+  enforce_private_link_endpoint_network_policies = true
 }
 
 resource "azurerm_virtual_network" "research_vnet" {
@@ -72,4 +88,6 @@ resource "azurerm_subnet" "research_system" {
   resource_group_name                           = azurerm_resource_group.nontnw.name
   virtual_network_name                          = azurerm_virtual_network.research_vnet.name
   address_prefixes                              = ["10.40.40.0/24"]
+  enforce_private_link_service_network_policies = true
+  enforce_private_link_endpoint_network_policies = true
 }
